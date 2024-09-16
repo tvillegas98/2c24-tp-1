a9e15d09f76643906a04e0c4e5ee63d165f3ba3b
Danikim01 <dakim@fi.uba.ar>
2024-09-15 21:23:20 -0300

    apis implementadas

--- app/app.js
+++ app/app.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 import express from 'express';
+import https from 'https'; 
 
 const app = express();
+
+const agent = new https.Agent({
+    rejectUnauthorized: false,
+});
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -14,18 +19,36 @@ app.get('/api/dictionary/:word', async (req, res) => {
     try{
         const word = req.params.word;
         const response = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
-        res.send(response.data);
+        const phonetics = response.data[0].phonetics
+        const meanings = response.data[0].meanings
+        res.send({word,phonetics, meanings});
     }catch(error){
         res.status(404).send('Word not found');
     }
 });
 
-app.get('/api/spaceflight_news', (req, res) => {
-    res.send('Spaceflight news');
+app.get('/api/spaceflight_news', async (req, res) => {
+    try{
+        const response = await axios.get('https://api.spaceflightnewsapi.net/v4/articles/?limit=5');
+        const titles = []
+        response.data.results.forEach((article) => {
+            titles.push(article.title);
+        });
+        res.send(titles);
+    }catch(error){
+        res.status(404).send('News not found');
+    }
 });
 
-app.get('/api/quote', (req, res) => {
-    res.send('Quote');
+app.get('/api/quote', async (req, res) => {
+    try{
+        const response = await axios.get('https://api.quotable.io/random', { httpsAgent: agent });
+        const content = response.data.content;
+        const author = response.data.author;
+        res.send({content, author});
+    }catch(error){
+        res.status(404).send('Quote not found');
+    }
 });
 
 const PORT = 3000
