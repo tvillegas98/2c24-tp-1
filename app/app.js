04a5cebd3a6c42360345224db5bb1538e00b055f
Danikim01 <dakim@fi.uba.ar>
2024-09-27 15:09:31 -0300

    endpoints separados en routers y router apiv2 agregado

--- app/app.js
+++ app/app.js
@@ -1,15 +1,18 @@
-import axios from 'axios';
+
 import express from 'express';
 import https from 'https';
-import { createClient } from 'redis';
+//import { createClient } from 'redis';
 
-const client = createClient({
-    url: 'redis://redis:6379'
-  });
+import api_router from './routers/api.router.js';
+import apiv2_router from './routers/apiv2.router.js';
 
-client.on('error', err => console.log('Redis Client Error', err));
+// const client = createClient({
+//     url: 'redis://redis:6379'
+//   });
 
-await client.connect();
+//client.on('error', err => console.log('Redis Client Error', err));
+
+//await client.connect();
 
 const app = express();
 
@@ -20,66 +23,13 @@ const agent = new https.Agent({
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.get('/api/ping', (req, res) => {
-    res.send('ping');
-});
 
-function handle_error(error) {
-    if (error.response) {
-        console.log(error.response.statusText);
-        console.log(error.response.status);
-        return [error.response.status, error.response.statusText]
-    } else if (error.request) {
-        console.log(error.message);
-        return [503, "API Connection Error"]
-    } else {
-        console.log('Error', error.message);
-        return [500, "Server Error"]
-    }
-}
+//endpoints sin el uso de redis
+app.use("/api",api_router);
 
-app.get('/api/dictionary', async (req, res) => {
-    try{
-        const word = req.query.word;
-        const response = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
-        const phonetics = response.data[0].phonetics
-        const meanings = response.data[0].meanings
-        res.send({word,phonetics, meanings});
-    }catch(error){
-        console.log(req.path);
-        console.log(req.query.word);
-        const response = handle_error(error)
-        res.status(response[0]).send(response[1]);
-    }
-});
+//endpoints con el uso de redis
+app.use("/apiv2",apiv2_router);
 
-app.get('/api/spaceflight_news', async (req, res) => {
-    try{
-        const response = await axios.get('https://api.spaceflightnewsapi.net/v4/articles/?limit=5');
-        const titles = []
-        response.data.results.forEach((article) => {
-            titles.push(article.title);
-        });
-        res.send(titles);
-    }catch(error){
-        console.log(req.path);
-        const response = handle_error(error)
-        res.status(response[0]).send(response[1]);
-    }
-});
-
-app.get('/api/quote', async (req, res) => {
-    try{
-        const response = await axios.get('https://api.quotable.io/random', { httpsAgent: agent });
-        const content = response.data.content;
-        const author = response.data.author;
-        res.send({content, author});
-    }catch(error){
-        console.log(req.path);
-        const response = handle_error(error)
-        res.status(response[0]).send(response[1]);
-    }
-});
 
 const PORT = 3000
 
@@ -87,6 +37,6 @@ app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
 });
 
-await client.set('key', 'value');
-const value = await client.get('key');
-console.log(value)
+// await client.set('key', 'value');
+// const value = await client.get('key');
+// console.log(value)
---
+++ app/routers/apiv2.router.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import { Router } from 'express';
+import handle_error from '../handle_error.js';
+
+const router = Router();
+
+router.get('/ping', (req, res) => {
+    res.send('ping');
+});
+
+router.get('/dictionary', async (req, res) => {
+    try{
+        const word = req.query.word;
+        const response = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+        const phonetics = response.data[0].phonetics
+        const meanings = response.data[0].meanings
+        res.send({word,phonetics, meanings});
+    }catch(error){
+        console.log(req.path);
+        console.log(req.query.word);
+        const response = handle_error(error)
+        res.status(response[0]).send(response[1]);
+    }
+});
+
+router.get('/spaceflight_news', async (req, res) => {
+    try{
+        const response = await axios.get('https://api.spaceflightnewsapi.net/v4/articles/?limit=5');
+        const titles = []
+        response.data.results.forEach((article) => {
+            titles.push(article.title);
+        });
+        res.send(titles);
+    }catch(error){
+        console.log(req.path);
+        const response = handle_error(error)
+        res.status(response[0]).send(response[1]);
+    }
+});
+
+router.get('/quote', async (req, res) => {
+    try{
+        const response = await axios.get('https://api.quotable.io/random', { httpsAgent: agent });
+        const content = response.data.content;
+        const author = response.data.author;
+        res.send({content, author});
+    }catch(error){
+        console.log(req.path);
+        const response = handle_error(error)
+        res.status(response[0]).send(response[1]);
+    }
+});
+
+export default router;
